210782ac9ba52ca4a8f8b329ed1cddbfb2f0ee23
kazuhira <kazuhira.m@gmail.com>
2014-04-26 00:40:04 +0900

    Distributed Callable implements HazelcastInstanceAware

--- hazelcast-data-affinity/src/main/scala/org/littlewings/hazelcast/affinity/DataAffinityTask.scala
+++ hazelcast-data-affinity/src/main/scala/org/littlewings/hazelcast/affinity/DataAffinityTask.scala
@@ -2,18 +2,20 @@ package org.littlewings.hazelcast.affinity
 
 import java.util.concurrent.Callable
 
-import com.hazelcast.core.{Hazelcast, PartitionAware}
+import com.hazelcast.core.{Hazelcast, HazelcastInstance, HazelcastInstanceAware, PartitionAware}
 
 @SerialVersionUID(1L)
 class DataAffinityTask(keyPrefix: String, range: Integer) extends Callable[Integer]
                                                           with PartitionAware[String]
+                                                          with HazelcastInstanceAware
                                                           with Serializable {
+  private var hazelcast: HazelcastInstance = _
+
+  override def setHazelcastInstance(hazelcast: HazelcastInstance): Unit =
+    this.hazelcast = hazelcast
+
   @throws(classOf[Exception])
   override def call: Integer = {
-    val threadName = Thread.currentThread.getName
-    val instanceName = threadName.substring(3, threadName.indexOf('.', 3))
-    val hazelcast = Hazelcast.getHazelcastInstanceByName(instanceName)
-
     val map = hazelcast.getMap[AffinityKey, Integer]("map")
 
     (1 to range).foldLeft(0) { (acc, i) =>
